956b0377763f89880e492d6858ab73017d1d462e
OOOWWWAAADDD <s1323123@cst.otsuma.ac.jp>
2025-06-17 11:48:03 +0900

    Update kadai_08.py

--- kadai_08.py
+++ kadai_08.py
@@ -17,15 +17,14 @@ passcode = st.text_input("パスワードを入力してください", type="pas
 if passcode:
     st.session_state.count += 1 
 
-if passcode :
+if passcode:
     if passcode == "WebProgramming":
         st.success('ログイン成功！')
         st.write(f"あなたは{st.session_state.count}回で突破に成功しました")
+        st.markdown("パスワードを知る方法は二つあります")
         st.balloons()
     else:
         st.error('パスワードが間違っています')
-
-        
 
 if st.button("パスワードを忘れた場合", key="forgot_password_button"):
     st.session_state.forgot_password = True
@@ -44,9 +43,8 @@ if st.session_state.forgot_password:
             with col2:
                 img_pass = st.text_input('画像の数字を入力してください', key='img_pass')
         else:
-            img_pass = None  # チェックしてなければ値なし
+            img_pass = None
         
-        # img_passが入力されているかつロボットチェックOKならボタンを表示
         if check and img_pass:
             try:
                 if int(img_pass) == 8:
@@ -60,57 +58,49 @@ if st.session_state.forgot_password:
             except ValueError:
                 st.error("画像の数字を入力してください")
 
-
-
-      #  mail_a = st.text_input("メールアドレスを入力してください")
-      #  aa_button = st.button("送信する")
-       # if aa_button:
-          #  if mail_a == "s1313@cst.otsuma.ac.jp":
-        #        print(st.session_state.mail)
-   #             st.write(st.session_state.mail)
-    #            check = st.checkbox('私はロボットではありません')
-    #            if check:
-    #                st.text('Checkbox A has checked')
-      #              img = Image.open('8.jpg')
-     #               st.image(img, caption='image file', width=100)
-      #      else:
-       #         st.error("メールアドレスにエラーがあります")
-#
-
-
-
-
 st.sidebar.write("大妻花子さん、ようこそ")
 with st.sidebar.expander("オプションを開く"):
     st.markdown('学生証情報表示')
     name = st.text_input("名前を入力してください")
 
-if name :
+if name:
     if name == "大妻花子":
         st.sidebar.success('大妻花子生年月日:1916年11月20日  \n学部:社会情報学部社会情報学科情報デザイン専攻  \n学籍番号:1313   \nメールアドレス:  \ns1313@cst.otsuma.ac.jp')
     else:
         st.sidebar.error('データが見つかりません')
 
 with st.sidebar.expander("お問い合わせフォーム"):
-    with st.sidebar.expander("パスワードお問い合わせ"):
-        mail = st.text_input("メールアドレスを入力してください")
-        passbutton = st.button("送信する")
+    with st.sidebar.expander("お問い合わせ"):
 
-if mail and passbutton:
-    if mail == "s1313@cst.otsuma.ac.jp":
-        selected_item = st.sidebar.radio(
-                  'お問い合わせ内容を選択してください。',
-                  ['成績開示', 'パスワード請求','その他']
-                )
-        a_button = st.sidebar.button('送信')
+        if "mail_sidebar" not in st.session_state:
+            st.session_state.mail_sidebar = ""
+        if "show_inquiry_form" not in st.session_state:
+            st.session_state.show_inquiry_form = False
+        if "selected_item" not in st.session_state:
+            st.session_state.selected_item = "成績開示"
 
-        if selected_item  == '成績開示' and a_button:
-            st.sidebar.info("提示可能な成績がありません")
-        elif selected_item == 'パスワード請求'and a_button:
-            st.sidebar.info("パスワードはWebProgrammingです")
-        elif selected_item == 'その他' and a_button:
-            st.sidebar.error("その他お問い合わせは現在ご利用できません")
-    else:
-        st.sidebar.error('メールアドレスにエラーがあります')  
+        mail = st.text_input("メールアドレスを入力してください", key="mail_sidebar_input")
+        passbutton = st.button("送信する", key="submit_email_button")
 
+        if passbutton:
+            if mail == "s1313@cst.otsuma.ac.jp":
+                st.session_state.mail_sidebar = mail
+                st.session_state.show_inquiry_form = True
+            else:
+                st.sidebar.error('メールアドレスにエラーがあります')
 
+        if st.session_state.show_inquiry_form:
+            st.session_state.selected_item = st.radio(
+                'お問い合わせ内容を選択してください。',
+                ['成績開示', 'パスワード請求', 'その他'],
+                key="sidebar_radio"
+            )
+            a_button = st.button("送信", key="inquiry_send_button")
+
+            if a_button:
+                if st.session_state.selected_item == '成績開示':
+                    st.sidebar.info("提示可能な成績がありません")
+                elif st.session_state.selected_item == 'パスワード請求':
+                    st.sidebar.info("パスワードはWebProgrammingです")
+                elif st.session_state.selected_item == 'その他':
+                    st.sidebar.error("その他お問い合わせは現在ご利用できません")
